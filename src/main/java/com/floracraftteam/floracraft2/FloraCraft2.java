e6672c3e93475ac8357b331490e57a88db0c1d34
nerdyturtle <nerdyturtle.mc@gmail.com>
2014-06-23 22:18:22 -0400

    Initial base class

---
+++ src/main/java/com/floracraftteam/floracraft2/FloraCraft2.java
@@ -0,0 +1,27 @@
+package com.floracraftteam.floracraft2;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid="FloraCraft2", name="FloraCraft 2", version="1.7.2-1.0")
+public class FloraCraft2 {
+	
+	@Mod.Instance
+	public static FloraCraft2 instance;
+	
+	@Mod.EventHandler
+	public void preInit(FMLPreInitializationEvent event){
+		
+	}
+	@Mod.EventHandler
+	public void init(FMLInitializationEvent event){
+		
+	}
+	@Mod.EventHandler
+	public void postInit(FMLPostInitializationEvent event){
+		
+	}
+
+}
